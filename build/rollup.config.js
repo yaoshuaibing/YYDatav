fcc085dc912a372411a85ff23fbe650adcb857a7
@yaosb <yaosb@soyuan.com.cn>
2020-08-26 17:52:45 +0800

    环境依赖

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,7 +1,23 @@
+import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
+const resolve = require('rollup-plugin-node-resolve')
+import babel from 'rollup-plugin-babel'
 export default {
-  input: './index.js',
+  input: 'src/App.vue',
   output: {
     file: 'dist/index.js',
-    format: 'umd'
-  }
+    format: 'umd',
+    name:'vue-rollup-conponent-template'
+
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    vue(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+  ],
+  external: ['Vue']
+
 }
